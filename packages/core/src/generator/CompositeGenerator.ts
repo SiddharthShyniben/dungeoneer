ba0af5dbcfa65db5cec0e4a9d604bc20b2ac5b3d
Siddharth Shyniben <siddharth.muscat@gmail.com>
2026-04-24 17:11:58 +0530

    feat: Implement composite generator

--- packages/core/src/generator/CompositeGenerator.ts
+++ packages/core/src/generator/CompositeGenerator.ts
@@ -4,13 +4,45 @@ import type { Region } from "../region/Region.ts";
 import type { RNG } from "../rng/RNG.ts";
 import type { Generator, GeneratorResult } from "./Generator.ts";
 
-export class CompositeGenerator implements Generator {
+interface CompositeGeneratorResult {
+    subResults: GeneratorResult[];
+}
+
+export class CompositeGenerator {
     constructor(
-        private partitioner: (region: Region, rng: RNG) => Region[],
-        private generators: Generator[]  // one per sub-region
+        private partitioner: (region: Region, rng: RNG) => [Region[], number[]],
+        private generators: Generator[]
     ) { }
 
-    generate(grid: Grid, region: Region, rng: RNG): GeneratorResult {
-        throw new Error("Not implemented");
+    generate(grid: Grid, region: Region, rng: RNG): CompositeGeneratorResult {
+        const [subregions, generatorIndices] = this.partitioner(region, rng);
+        const results: GeneratorResult[] = [];
+
+        for (let i = 0; i < subregions.length; i++) {
+            const subregion = subregions[i];
+
+            if (!subregion) {
+                throw new Error(`No subregion provided for index ${i}.`);
+            }
+
+            const generatorIndex = generatorIndices[i];
+
+            if (typeof generatorIndex === "undefined") {
+                throw new Error(`No generator index provided for subregion ${i}.`);
+            }
+
+            const generator = this.generators[generatorIndex];
+
+            if (!generator) {
+                throw new Error(`No generator found at index ${generatorIndex} for subregion ${i}.`);
+            }
+
+            const result = generator.generate(grid, subregion, rng);
+            results.push(result);
+        }
+
+        return {
+            subResults: results
+        };
     }
 }
