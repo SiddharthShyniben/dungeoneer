1345aefd1cb25b64bb1dd32316fea7d332db058c
Siddharth Shyniben <siddharth.muscat@gmail.com>
2026-04-24 14:10:37 +0530

    feat: Implement basic grid functions

--- packages/core/src/grid/Grid.ts
+++ packages/core/src/grid/Grid.ts
@@ -1,19 +1,40 @@
+import { BaseTile } from "./BaseTile.js";
 import type { Cell } from "./Cell.ts";
 
 export class Grid<T = never> {
-    private cells: Cell<T>[][]
+    private cells: Cell<T>[][] = [];
 
-    constructor(public width: number, public height: number) {}
+    constructor(public width: number, public height: number) {
+        this.initializeCells();
+    }
 
     get(x: number, y: number): Cell<T> | undefined {
-        throw new Error("Not implemented");
+        if (!this.inBounds(x, y)) {
+            return undefined;
+        }
+
+        return this.cells[y]![x];
     }
 
     set(x: number, y: number, cell: Cell<T>): void {
-        throw new Error("Not implemented");
+        if (!this.inBounds(x, y)) {
+            throw new Error("Coordinates out of bounds");
+        }
+
+        this.cells[y]![x] = cell;
     }
 
     inBounds(x: number, y: number): boolean {
-        throw new Error("Not implemented");
+        return x >= 0 && x < this.width && y >= 0 && y < this.height;
+    }
+
+    private initializeCells(): void {
+        for (let y = 0; y < this.height; y++) {
+            const row: Cell<T>[] = [];
+            for (let x = 0; x < this.width; x++) {
+                row.push({base: BaseTile.Void});
+            }
+            this.cells.push(row);
+        }
     }
 }
