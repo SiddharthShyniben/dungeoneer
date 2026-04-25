7361fa05ae5ef748e46569539ded5323d42c0526
Siddharth Shyniben <siddharth.muscat@gmail.com>
2026-04-25 07:59:01 +0530

    feat: Test advanced partitioner

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,12 +1,27 @@
-import { DefaultRNG, Cave, Grid, createFilledRegion, generatorId } from "@dungeoneer/core";
+import { DefaultRNG, Cave, Grid, generatorId, CompositeGenerator, Connector, NearestNeighborStrategy, DrunkardCarver, runPipeline, DiagonalPartitioner } from "@dungeoneer/core";
 import { render } from "./renderer.ts";
 
-const grid = new Grid(100, 100);
-const region = createFilledRegion(100, 100);
-
+const grid = new Grid(200, 100);
 const rng = new DefaultRNG("seed");
-const generator = new Cave(generatorId());
+const partitioner = new DiagonalPartitioner();
 
-generator.generate(grid, region, rng);
+const generator = new CompositeGenerator(
+    (region, rng) => {
+        const regions = partitioner.partition(region, rng);
+        return [regions, regions.map((_, i) => i % 2)]
+    },
+    [new Cave(generatorId()), new Cave(generatorId(), 0.3)]
+);
+
+generator.generate(grid, rng);
+
+const pipeline = [
+    new Connector(
+        new NearestNeighborStrategy(),
+        new DrunkardCarver(0.4)
+    )
+];
+
+runPipeline(grid, rng, pipeline)
 
 render(grid);
